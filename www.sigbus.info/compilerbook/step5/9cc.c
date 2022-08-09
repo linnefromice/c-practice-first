c696e928433ed116527e9e24eb49fd1db44aaa9b
linnefromice <linnefromice@gmail.com>
2022-08-09 14:46:34 +0900

    update: step5 - add generator

--- www.sigbus.info/compilerbook/step5/9cc.c
+++ www.sigbus.info/compilerbook/step5/9cc.c
@@ -30,6 +30,20 @@ Node *new_node_num(int val) {
   node->val = val;
   return node;
 }
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number());
+}
+
 Node *mul() {
   Node *node = primary();
 
@@ -54,4 +68,64 @@ Node *expr() {
     else
       return node;
   }
+}
+
+//
+// Code generator
+//
+
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+      break;
+  }
+
+  printf("  push rax\n");
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    error("引数の個数が正しくありません");
+    return 1;
+  }
+
+  // トークナイズしてパースする
+  user_input = argv[1];
+  token = tokenize(user_input);
+  Node *node = expr();
+
+  // アセンブリの前半部分を出力
+  printf(".intel_syntax noprefix\n");
+  printf(".globl main\n");
+  printf("main:\n");
+
+  // 抽象構文木を下りながらコード生成
+  gen(node);
+
+  // スタックトップに式全体の値が残っているはずなので、それを RAX にロードして関数からの返り値とする
+  printf("  pop rax\n");
+  printf("  ret\n");
+  return 0;
 }
