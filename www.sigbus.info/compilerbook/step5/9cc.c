3cae863239e4608fe050c197f128e957fc166f43
linnefromice <linnefromice@gmail.com>
2022-08-09 11:31:08 +0900

    update: step5 - add func to generate new_node

--- www.sigbus.info/compilerbook/step5/9cc.c
+++ www.sigbus.info/compilerbook/step5/9cc.c
@@ -15,3 +15,18 @@ struct Node {
   Node *rhs; // 右辺
   int val;
 };
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
