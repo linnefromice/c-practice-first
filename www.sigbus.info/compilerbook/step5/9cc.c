ab11735dcd461bae38e2785dbefae923b00c35ed
linnefromice <linnefromice@gmail.com>
2022-08-09 11:29:17 +0900

    update: step5 - add Node struct

---
+++ www.sigbus.info/compilerbook/step5/9cc.c
@@ -0,0 +1,17 @@
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // 整数
+} NodeKind;
+
+// 抽象構文木のノードの型
+typedef struct Node Node;
+struct Node {
+  NodeKind kind; // ノードの型
+  Node *lhs; // 左辺
+  Node *rhs; // 右辺
+  int val;
+};
